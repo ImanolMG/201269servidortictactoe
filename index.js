5d2d73d199ec45e07c71118ba20d7e4be08f7430
ImanolMG <201269@ids.upchiapas.edu.mx>
2021-07-28 12:26:50 -0500

    Subida del proyecto

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server} = require('socket.io')
+const io = new Server(server, {
+    cors: {
+        methods: ["GET", "POST"]
+    }
+})
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Hola Mundo</h1>')
+})
+var cont=0;
+io.on('connection', socket =>{
+    console.log('Nueva conexión', socket.id)
+    if(cont%2==0){
+        socket.emit('valorJugador', {xOo:"X"})
+    }
+    else{
+        socket.emit('valorJugador', {xOo:"O"})
+    }
+   cont++;
+console.log(cont)
+    socket.on('message', (data) => {
+        socket.emit('Server:message', {name: socket.id, message:data})
+    })
+    socket.on('espacio', (data)=>{
+        console.log(data.b)
+        io.emit('asigEspacio', {esp:data.b})
+        
+        })
+        socket.on('valorEspacio', (data)=>{
+            console.log(data.v)
+            io.emit('asigValorEspacio', {vesp:data.v})
+            
+            })
+        
+})
+
+server.listen(3000, ()=>{console.log('Server inicializado')})
